59b49bb9dbbf8070f6caabc98f48cd7f91137b2d
luizhp <luizhp@yahoo.com>
2025-05-11 19:58:13 -0300

    feat: adds checks for media file existence and validity.

--- TranscriptionConfig.py
+++ TranscriptionConfig.py
@@ -15,3 +15,9 @@ class TranscriptionConfig:
     model_name: str = "tiny"
     media_files: list = None
     model: object = None
+
+  # def getTargetFile(self) -> str:
+  #     # Output transcribed file
+  #     lrc_file_path_alongside_media = os.path.splitext(media_file_path)[0] + f".{self.targettype}"
+  #     abs_lrc_file_path = os.path.abspath(lrc_file_path_alongside_media)
+  #     return abs_lrc_file_path
--- transcriber.py
+++ transcriber.py
@@ -11,9 +11,38 @@ def transcribe_media_to_lrc(config : TranscriptionConfig, media_file_path: str):
     """
     # Input media file
     abs_media_file_path = os.path.abspath(media_file_path)
+
+    # Check if the media file exists
+    if not os.path.exists(abs_media_file_path):
+        log(f"Media file does not exist: {abs_media_file_path}")
+        return
+
+    # Check if the media file is a valid audio file
+    if not os.path.isfile(abs_media_file_path):
+        log(f"Media file is not a valid file: {abs_media_file_path}")
+        return
+
     # Output transcribed file
-    lrc_file_path_alongside_media = os.path.splitext(media_file_path)[0] + f".{config.targettype}"
+    src_lng = ""if config.sourcelanguage is None else f"_{config.sourcelanguage}"
+    lrc_file_path_alongside_media = os.path.splitext(media_file_path)[0] + src_lng + f".{config.targettype}"
     abs_lrc_file_path = os.path.abspath(lrc_file_path_alongside_media)
+
+    # Check if the output file already exists
+    match config.targetexists:
+        case 'skip':
+            if os.path.exists(abs_lrc_file_path):
+                log(f"Skipping existing file: {abs_lrc_file_path}")
+                return
+        case 'rename':
+            base, ext = os.path.splitext(abs_lrc_file_path)
+            i = 1
+            while os.path.exists(abs_lrc_file_path):
+                abs_lrc_file_path = f"{base}_{i}{ext}"
+                i += 1
+            log(f"Avoiding collision by renaming existing file to: {abs_lrc_file_path}")
+        case 'overwrite':
+            log(f"Overwriting existing file: {abs_lrc_file_path}")
+            pass
 
     log(f"Transcribing {abs_media_file_path} to {abs_lrc_file_path}")
 
@@ -47,21 +76,12 @@ def transcribe_media_to_lrc(config : TranscriptionConfig, media_file_path: str):
               log("WARNING: json format not supported yet")
               pass
 
-    # Output the transcription to a file
     if transcribe_content == []:
         log(f"ERROR: Transcription failed {abs_media_file_path}: no segments found")
         return
-    if config.targetexists == "skip" and os.path.exists(abs_lrc_file_path):
-        log(f"ERROR: Transcription failed {abs_media_file_path}: file already exists")
-        return
-    if config.targetexists == "rename":
-        base, ext = os.path.splitext(abs_lrc_file_path)
-        i = 1
-        while os.path.exists(abs_lrc_file_path):
-            abs_lrc_file_path = f"{base}_{i}{ext}"
-            i += 1
-    if config.targetexists == "overwrite":
-        pass
+
+    # Output the transcription to a file
+    log("Writing to file...")
     with open(abs_lrc_file_path, 'w', encoding='utf-8') as f:
         for line in transcribe_content:
             f.write(line + "\n")
