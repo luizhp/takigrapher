caf5e131e072b2b209e18289fc0418a62bcf98e0
luizhp <luizhp@yahoo.com>
2025-05-11 10:52:11 -0300

    feat: adds CLI argument parsing for transcription configuration

---
+++ TranscriptionConfig.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+import os
+
+@dataclass
+class TranscriptionConfig:
+    device: str = "cpu"
+    verbose: bool = False
+    sourcetype: str = "all"
+    sourcelanguage: str = "en"
+    targetlanguage: str = "en"
+    targettype: str = "lrc"
+    targetexists: str = "overwrite"
+    targetsuffix: bool = True
+    media_search_folder: str = "./media"
+    model_name: str = "tiny"
+    media_files: list = None
+    model: object = None
+
+    def media_search_folder_abs(self) -> str:
+        """Returns the absolute path of the media_search_folder."""
+        return os.path.abspath(self.media_search_folder)
